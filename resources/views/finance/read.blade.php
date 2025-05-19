b59e7307f0a40dfae818146e75e8c5ce28152369
Maria Siboro <mariaboro01@gmail.com>
2025-05-19 21:30:03 +0700

    revisi date task

--- resources/views/finance/read.blade.php
+++ resources/views/finance/read.blade.php
@@ -20,12 +20,12 @@
         </a>
 
         <div class="relative inline-block text-left">
-          <button type="button" class="inline-flex justify-center w-full rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          <!-- <button type="button" class="inline-flex justify-center w-full rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
               <path fill-rule="evenodd" d="M12 2.25a.75.75 0 0 1 .75.75v11.69l3.22-3.22a.75.75 0 1 1 1.06 1.06l-4.5 4.5a.75.75 0 0 1-1.06 0l-4.5-4.5a.75.75 0 1 1 1.06-1.06l3.22 3.22V3a.75.75 0 0 1 .75-.75Zm-9 13.5a.75.75 0 0 1 .75.75v2.25a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5V16.5a.75.75 0 0 1 1.5 0v2.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V16.5a.75.75 0 0 1 .75-.75Z" clip-rule="evenodd" />
             </svg>
             <span class="sr-only">Export</span>
-          </button>
+          </button> -->
           <div class="dropdown-content mt-2 absolute hidden bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
             <button class="block text-left w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
               Export sebagai PDF
@@ -105,13 +105,13 @@
       <div class="bg-white shadow rounded p-4">
         <h2 class="text-xl font-semibold mb-1">All transaction</h2>
         <p class="text-sm text-gray-500 mb-4">Daftar semua transaksi dalam periode yang dipilih</p>
-        <div class="flex items-center space-x-2 mb-4">
+        <!-- <div class="flex items-center space-x-2 mb-4">
           <button class="border px-3 py-1 rounded">Filter</button>
           <button class="bg-gray-200 px-3 py-1 rounded">All</button>
           <button class="bg-green-100 text-green-600 px-3 py-1 rounded">↑ Revenue</button>
           <button class="bg-red-100 text-red-600 px-3 py-1 rounded">↓ Expense</button>
         </div>
-        <input type="text" placeholder="Cari transaksi..." class="w-full border px-3 py-2 rounded mb-4">
+        <input type="text" placeholder="Cari transaksi..." class="w-full border px-3 py-2 rounded mb-4"> -->
 
         <div class="overflow-x-auto">
           <table class="w-full table-auto text-left text-sm">
