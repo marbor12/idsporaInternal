83330788ca94f32f8f96779d5b41cdd2f8335a03
Maria Siboro <mariaboro01@gmail.com>
2025-06-11 16:04:44 +0700

    task

--- database/migrations/2025_06_07_060122_needs.php
+++ database/migrations/2025_06_07_060122_needs.php
@@ -4,13 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('needs', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('event_id');
+            $table->string('title');
+            // $table->string('category');
+            $table->enum('category', [
+                'logistik',
+                'desain & media',
+                'dokumentasi & administrasi',
+                'konsumsi & sdm'
+            ]);
             $table->string('description');
             $table->string('status')->default('draft'); // draft, submitted_to_ceo, approved_by_ceo, rejected_by_ceo, etc
             $table->text('approval_notes')->nullable();
